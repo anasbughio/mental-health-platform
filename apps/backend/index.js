db113244feacd13866ffd83ab3701e5183152759
anasbughio <anasbughio@gmail.com>
2026-03-04 23:54:28 +0500

    fix: remove nested git repo and track backend files

---
+++ apps/backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// We map port 3000 to port 80 in our GitHub Actions SSM command
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Mental Health Platform API is running. Access /api/health for status.');
+});
+
+// Standard DevOps Health Check Route
+app.get('/api/health', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'The containerized API is live and responding!',
+        timestamp: new Date().toISOString()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening  port ${PORT}`);
+});
