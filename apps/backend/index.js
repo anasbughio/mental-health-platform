327e0bdbd267e11d15333759d6355db87efe897b
anasbughio <anasbughio@gmail.com>
2026-03-05 00:00:44 +0500

    test

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -21,5 +21,5 @@ app.get('/api/health', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening  port ${PORT}`);
+    console.log(`Server is listening on  port ${PORT}`);
 });
