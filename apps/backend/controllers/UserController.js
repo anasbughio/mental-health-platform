8b11d86e68378b493bd817e4464c41a71fbd2262
anasbughio <anasbughio@gmail.com>
2026-03-06 23:39:53 +0500

    debug

--- apps/backend/controllers/UserController.js
+++ apps/backend/controllers/UserController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 exports.createUser = async (req,res) => {
 
  const {name,email,password,role} = req.body;
@@ -8,7 +8,7 @@ exports.createUser = async (req,res) => {
     const existingUser =  await User.findOne({email});
     if(existingUser)   res.status(400).json({status:'error',message:'Email already in use'});
 
-    const hashedPassword = await bcrytp.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password,10);
 
     const newUser  = await User.create({
         name,
