aaebc1c89ee1db3c092abdb19cb71fafad67c627
anasbughio <anasbughio@gmail.com>
2026-03-09 23:25:02 +0500

    api changes

--- apps/backend/routes/MoodRoutes.js
+++ apps/backend/routes/MoodRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {authMiddleware} = require('../middleware/auth');
 const {createMoodLog, getMyMoodLogs} = require('../controllers/MoodLogController');
+const {createMood} = require('../controllers/moodController');
 
 
-router.post('/',authMiddleware,createMoodLog);
+router.post('/',authMiddleware,createMood);
 router.get('/',authMiddleware,getMyMoodLogs);
 
 
